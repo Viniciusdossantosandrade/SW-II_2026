43459d6b11c4357cd619f76a1624990c2484fb6e
Viniciusdossantosandrade <162480502+Viniciusdossantosandrade@users.noreply.github.com>
2026-02-10 17:31:20 -0300

    Add files via upload

---
+++ aula_01/exemplo2.php
@@ -0,0 +1,12 @@
+<?php
+
+ $nome = "Vinicius";
+ $idade = 16;
+ $preco = 10.50;
+ $ativo = true;
+ echo "Nome: " . $nome . "<br>";
+ echo "idade: " . $idade . "<br>";
+ echo "Preco: " . $preco . "<br>";
+ echo "Status: " . ($ativo ? "Ativo" : "Inativo");
+ 
+ ?>
